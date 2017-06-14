0610cbec79c7b29d36680ef453d1902e1b2e9153
RakovBogdan <rakovbogdan@gmail.com>
2017-06-14 20:15:49 +0300

    sha256 implemented

--- hashes/models.py
+++ hashes/models.py
@@ -5,3 +5,6 @@ class Document(models.Model):
     document = models.FileField(upload_to='./')
     sha256 = models.CharField(max_length=256)
     # uploaded_count = models.DecimalField()
+
+    def filepath(self):
+        return self.document.path
--- hashes/views.py
+++ hashes/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
 from .forms import DocumentForm, Document
+import hashlib
 
 
 def model_form_upload(request):
-    documents = Document.objects.all()
     if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            doc = form.save()
+            file_path = doc.filepath()
+            sha_256 = hashlib.sha256(open(file_path, 'rb').read()).hexdigest()
+            doc.sha256 = sha_256
+            doc.save()
     else:
         form = DocumentForm()
+
+    documents = Document.objects.all()
     return render(request, 'hashes/model_form_upload.html', {
         'form': form,
         'documents': documents,
